6d3ef32e67c8a684fb5ddb5eaa46c25e902af3b6
MOHD YUSUF <mohdyusufr@gmail.com>
2025-07-16 15:11:33 +0530

    Update version 1.0.1 -> 1.0.2

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,8 @@ android {
         applicationId = "com.zmq.blood_connect"
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 2
-        versionName = "1.0.1"
+        versionCode = 3
+        versionName = "1.0.2"
     }
 
     signingConfigs {
